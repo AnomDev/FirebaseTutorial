9530f844e4b6459d82a71d1d51a8e52d6130018f
AnomDev <serbla.dev@gmail.com>
2021-07-24 13:17:31 +0200

    Validations BASIC Auth (not complete - II)

--- app/src/main/java/com/anomdev/firebasetutorial/AuthActivity.kt
+++ app/src/main/java/com/anomdev/firebasetutorial/AuthActivity.kt
@@ -19,7 +19,6 @@ import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_auth.*
-import kotlinx.android.synthetic.main.activity_home.*
 import java.util.regex.Pattern
 
 class AuthActivity : AppCompatActivity() {
@@ -28,15 +27,14 @@ class AuthActivity : AppCompatActivity() {
     private val callBackManager = CallbackManager.Factory.create()
 
     //TODO: Esto me servirá para validar el formato del email pero falta implementarlo.
-    private val EMAIL_ADDRESS_PATTERN = Pattern.compile(
-        "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
-                "\\@" +
-                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
-                "(" +
-                "\\." +
-                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
-                ")+"
-    )
+    private val EMAIL_ADDRESS_PATTERN = "[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}" +
+            "\\@" +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+            "(" +
+            "\\." +
+            "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+            ")+"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -55,8 +53,10 @@ class AuthActivity : AppCompatActivity() {
         bundle.putString("message", "Integracion de Firebase completa")
         analytics.logEvent("InitScreen", bundle)
 
-        //Setup
+        //Registros y login
         setup()
+
+        //Comprobación de sesión abierta anterior
         session()
     }
 
@@ -66,24 +66,91 @@ class AuthActivity : AppCompatActivity() {
 
         //Aquí está la lógica para el registro por medio de usuario y contraseña
         signup_btn.setOnClickListener {
+            validateSignUpForm()
+
+            if (validateSignUpForm()) {
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    email_et.text.toString(),
+                    password_et.text.toString()
+                ).addOnCompleteListener {
+
+                    //Aquí nos notificará si la petición se ha cumplido satisfactoriamente
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+
+                    }
+                }
+            }
+        /*
+
+            //TODO: La gracia sería meter todas las validaciones en una función y en Setup() llamarla para no llenarlo todo de código
+            //if (validateForm())
+
 
             //Aquí realizaremos todas las comprobaciones de los campos email y password
-            if (email_et.text.isNotEmpty() && password_et.text.isNotEmpty()) {
+            if (email_et.text.isNotEmpty()
+                && email_et.text.isNotBlank()
+                && email_et.text.contains("@" + ".")
+                && password_et.text.isNotEmpty()
+                && password_et.text.isNotBlank()
+                && password_et.text.length >= 6
+            ) {
 
-                //En este caso sólo miramos que ambos textos no esten vacíos
-                FirebaseAuth.getInstance().createUserWithEmailAndPassword(email_et.text.toString(),
-                    password_et.text.toString()).addOnCompleteListener{
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    email_et.text.toString(),
+                    password_et.text.toString()
+                ).addOnCompleteListener {
 
-                        //Aquí nos notificará si la petición se ha cumplido satisfactoriamente
-                        if (it.isSuccessful){
-                            showHome(it.result?.user?.email ?: "",ProviderType.BASIC)
-                        } else {
-                            showAlert("Error en el registro","Algo ha fallado en el registro con usuario y contraseña. Vuelve a intentarlo más adelante.")
-                        }
+                    //Aquí nos notificará si la petición se ha cumplido satisfactoriamente
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                    } else {
+                        showAlert(
+                            "Error en el registro",
+                            "Algo ha fallado en el registro con usuario y contraseña. Vuelve a intentarlo más adelante."
+                        )
+                    }
                 }
             } else {
-                showAlert("Campos sin rellenar","Por favor, asegúrate de rellenar todos los campos")
-            }
+                if (!email_et.text.isNotEmpty()) {
+                    showAlert(
+                        "Email sin rellenar",
+                        "Por favor, asegúrate de rellenar el email"
+                    )
+
+                }
+                if (!email_et.text.isNotBlank()) {
+                    showAlert(
+                        "Espacios en blanco",
+                        "Por favor, asegúrate de no dejar espacios en blanco"
+                    )
+                }
+                if (!email_et.text.contains("@")) {
+                    showAlert(
+                        "Formato incorrecto",
+                        "El formato del email no tiene la sintáxis correcta. Revísalo."
+                    )
+                }
+                if (!password_et.text.isNotEmpty()) {
+                    showAlert(
+                        "Contraseña sin rellenar",
+                        "Por favor, asegúrate de rellenar la contraseña"
+                    )
+                }
+                if (!password_et.text.isNotBlank()) {
+                    showAlert(
+                        "Espacios en blanco",
+                        "La contraseña no puede tener espacios en blanco"
+                    )
+                }
+                if (password_et.text.length < 6) {
+                    showAlert(
+                        "Contraseña demasiado corta",
+                        "La contraseña debe tener como mínimo 6 caractéres"
+                    )
+                }
+
+            }*/
         }
 
         //Aquí está la lógica para el login mediante usuario y contraseña
@@ -93,23 +160,31 @@ class AuthActivity : AppCompatActivity() {
             if (email_et.text.isNotEmpty() && password_et.text.isNotEmpty()) {
 
                 //En este caso sólo miramos que ambos textos no esten vacíos
-                FirebaseAuth.getInstance().signInWithEmailAndPassword(email_et.text.toString(),
-                    password_et.text.toString()).addOnCompleteListener{
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    email_et.text.toString(),
+                    password_et.text.toString()
+                ).addOnCompleteListener {
 
                     //Aquí nos notificará si la petición se ha cumplido satisfactoriamente
-                    if (it.isSuccessful){
-                        showHome(it.result?.user?.email ?: "",ProviderType.BASIC)
+                    if (it.isSuccessful) {
+                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
                     } else {
-                        showAlert("Error en el login","Algo ha fallado en el login con usuario y contraseña. Vuelve a intentarlo más adelante.")
+                        showAlert(
+                            "Error en el login",
+                            "Algo ha fallado en el login con usuario y contraseña. Vuelve a intentarlo más adelante."
+                        )
                     }
                 }
-            } else{
-                showAlert("Campos sin rellenar","Por favor, asegúrate de rellenar todos los campos")
+            } else {
+                showAlert(
+                    "Campos sin rellenar",
+                    "Por favor, asegúrate de rellenar todos los campos"
+                )
             }
         }
 
         //Aquí está la lógica para el registro/login mediante Gmail
-        google_btn.setOnClickListener{
+        google_btn.setOnClickListener {
 
             //Configuración de la autenticación
             val googleConf = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -120,7 +195,10 @@ class AuthActivity : AppCompatActivity() {
             val googleClient = GoogleSignIn.getClient(this, googleConf)
             googleClient.signOut() //De esta manera cerramos la sesión que pueda estar iniciada antes cuando pinchamos el botón de Google.
 
-            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN) //Aquí iniciamos una activity pidiendo que nos responda algo (el id de autenticación GOOGLE_SIGN_IN")
+            startActivityForResult(
+                googleClient.signInIntent,
+                GOOGLE_SIGN_IN
+            ) //Aquí iniciamos una activity pidiendo que nos responda algo (el id de autenticación GOOGLE_SIGN_IN")
 
         }
 
@@ -134,43 +212,51 @@ class AuthActivity : AppCompatActivity() {
            usando la CMD en la ruta:
                  C:\Program Files\Java\jdk-16.0.2\bin>
         */
-        facebook_btn.setOnClickListener{
+        facebook_btn.setOnClickListener {
 
             LoginManager.getInstance().logInWithReadPermissions(this, listOf("email"))
 
             LoginManager.getInstance().registerCallback(callBackManager,
-            object : FacebookCallback<LoginResult> {
+                object : FacebookCallback<LoginResult> {
 
-                override fun onSuccess(result: LoginResult?) { //Aquí nos habríamos autenticado en el servidor de Facebook
-                    result?.let {
-                        val tokenAuthFacebook = it.accessToken
+                    override fun onSuccess(result: LoginResult?) { //Aquí nos habríamos autenticado en el servidor de Facebook
+                        result?.let {
+                            val tokenAuthFacebook = it.accessToken
 
-                        val credential = FacebookAuthProvider.getCredential(tokenAuthFacebook.token)
-                        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
-                            if (it.isSuccessful){
-                                showHome(it.result?.user?.email ?: "", ProviderType.FACEBOOK)
-                            } else {
-                                showAlert("Facebook Error","Algo ha fallado en el login con Facebook. Vuelve a intentarlo más adelante.")
-                            }
+                            val credential =
+                                FacebookAuthProvider.getCredential(tokenAuthFacebook.token)
+                            FirebaseAuth.getInstance().signInWithCredential(credential)
+                                .addOnCompleteListener {
+                                    if (it.isSuccessful) {
+                                        showHome(
+                                            it.result?.user?.email ?: "",
+                                            ProviderType.FACEBOOK
+                                        )
+                                    } else {
+                                        showAlert(
+                                            "Facebook Error",
+                                            "Algo ha fallado en el login con Facebook. Vuelve a intentarlo más adelante."
+                                        )
+                                    }
+                                }
                         }
                     }
-                }
 
-                override fun onCancel() {
+                    override fun onCancel() {
 
-                }
+                    }
 
-                override fun onError(error: FacebookException?) {
-                    showAlert()
-                }
-            })
+                    override fun onError(error: FacebookException?) {
+                        showAlert()
+                    }
+                })
         }
     }
 
 
     //En esta función realizamos la navegación entre pantallas (de Auth a Home) con los datos de mail y pass.
-    private fun showHome(email: String, provider: ProviderType){
-        val homeIntent = Intent(this, HomeActivity::class.java).apply{
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
             putExtra("email", email)
             putExtra("provider", provider.name)
         }
@@ -180,11 +266,11 @@ class AuthActivity : AppCompatActivity() {
     //Aquí recuperaremos si existe un email y un provider, lo que significaría que existe una sesión activa (user autenticado)
     private fun session() {
         val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
-        val email = prefs.getString("email",null)
-        val provider = prefs.getString("provider",null)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
 
         //Si existen mail y proveedor, no mostrará el layout de Auth y nos llevará a Home con showHome
-        if (email!= null && provider != null) {
+        if (email != null && provider != null) {
             auth_layout.visibility = View.INVISIBLE
             showHome(email, ProviderType.valueOf(provider))
         }
@@ -200,32 +286,37 @@ class AuthActivity : AppCompatActivity() {
 
     //Al iniciar una actividad pidiendole una respuesta, necesitamos sobreescribir onActivityResult
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        callBackManager.onActivityResult(requestCode,resultCode, data)
+        callBackManager.onActivityResult(requestCode, resultCode, data)
 
         super.onActivityResult(requestCode, resultCode, data)
 
         //Esta parte autentica en los servidores de GOOGLE.
-        if (requestCode == GOOGLE_SIGN_IN){
+        if (requestCode == GOOGLE_SIGN_IN) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data) //Recuperamos la respuesta
 
             // Cuando se llama a un ActivityResult, la línea de Account tiene que estar protegida contra nulos en el caso de
             // que no sea capaz de recuperar una cuenta. Por esta razón se encapsula en un try...catch
             try {
-                val account = task.getResult(ApiException::class.java) //Recuperamos la cuenta autenticada
+                val account =
+                    task.getResult(ApiException::class.java) //Recuperamos la cuenta autenticada
 
                 //Esta otra parte usa la anterior autenticación en GOOGLE para autenticarse en la consola de Firebase.
-                if (account != null){
+                if (account != null) {
                     val credential = GoogleAuthProvider.getCredential(account.idToken, null)
-                    FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener {
-                        if (it.isSuccessful){
-                            showHome(account.email ?: "", ProviderType.GOOGLE)
-                        } else {
-                            showAlert("Gmail Error","Algo ha fallado en el login con Gmail. Vuelve a intentarlo más adelante.")
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showHome(account.email ?: "", ProviderType.GOOGLE)
+                            } else {
+                                showAlert(
+                                    "Gmail Error",
+                                    "Algo ha fallado en el login con Gmail. Vuelve a intentarlo más adelante."
+                                )
+                            }
                         }
-                    }
 
                 }
-            } catch (e: ApiException){
+            } catch (e: ApiException) {
                 showAlert("Error TryCatch", "Error en el try...catch")
             }
 
@@ -233,12 +324,68 @@ class AuthActivity : AppCompatActivity() {
         }
     }
 
+    private fun validateSignUpForm(): Boolean{
+        //Aquí realizaremos todas las comprobaciones de los campos email y password
+        if (email_et.text.isNotEmpty()
+            && email_et.text.isNotBlank()
+            && email_et.text.contains("@")
+            && password_et.text.isNotEmpty()
+            && password_et.text.isNotBlank()
+            && password_et.text.length >= 6
+        ) {
+            return true
+        } else {
+            if (!email_et.text.isNotEmpty()) {
+                showAlert(
+                    "Email sin rellenar",
+                    "Por favor, asegúrate de rellenar el email"
+                )
+
+            }
+            if (!email_et.text.isNotBlank()) {
+                showAlert(
+                    "Espacios en blanco",
+                    "Por favor, asegúrate de no dejar espacios en blanco"
+                )
+            }
+            if (!email_et.text.contains("@")) {
+                showAlert(
+                    "Formato incorrecto",
+                    "El formato del email no tiene la sintáxis correcta. Revísalo."
+                )
+            }
+            if (!password_et.text.isNotEmpty()) {
+                showAlert(
+                    "Contraseña sin rellenar",
+                    "Por favor, asegúrate de rellenar la contraseña"
+                )
+            }
+            if (!password_et.text.isNotBlank()) {
+                showAlert(
+                    "Espacios en blanco",
+                    "La contraseña no puede tener espacios en blanco"
+                )
+            }
+            if (password_et.text.length < 6) {
+                showAlert(
+                    "Contraseña demasiado corta",
+                    "La contraseña debe tener como mínimo 6 caractéres"
+                )
+            }
+
+        }
+        return false
+    }
+
     // Creamos una función para mostrar un Alert si algo ha ido mal
-    private fun showAlert(title: String="Error", message: String="Algo ha ido mal. Pruebe de nuevo más adelante."){
+    private fun showAlert(
+        title: String = "Error",
+        message: String = "Algo ha ido mal. Pruebe de nuevo más adelante."
+    ) {
         val builder = AlertDialog.Builder(this)
         builder.setTitle(title)
         builder.setMessage(message)
-        builder.setPositiveButton("Aceptar",null)
+        builder.setPositiveButton("Aceptar", null)
 
         val dialog: AlertDialog = builder.create()
         dialog.show()
